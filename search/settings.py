37a5813267b402c2ec6da73e91e26cd4c6709a87
Daniel <albuquerquedaniel898@gmail.com>
2021-10-20 20:35:00 -0300

    static

--- search/settings.py
+++ search/settings.py
@@ -120,11 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/compsem/myproject/search/static/'
+STATIC_ROOT = 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "/home/compsem/myproject/search/static/",
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
