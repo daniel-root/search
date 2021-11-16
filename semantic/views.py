ea097c6df070b32e94ccd236b322c00f2373b1f4
Daniel <albuquerquedaniel898@gmail.com>
2021-11-16 16:45:03 -0300

    remove language_tool_python

--- semantic/views.py
+++ semantic/views.py
@@ -4,7 +4,7 @@ import spacy
 nlp = spacy.load('en_core_web_lg')
 from itertools import product
 from . import wordnet
-import language_tool_python
+#import language_tool_python
 import requests
 import csv
 
@@ -14,8 +14,8 @@ headers = {"Authorization": f"Bearer {API_TOKEN}"}
 sentence_transformers = ['sentence-transformers/all-mpnet-base-v2', 'sentence-transformers/clip-ViT-B-32', 'sentence-transformers/msmarco-bert-co-condensor', 'sentence-transformers/msmarco-distilbert-cos-v5', 'sentence-transformers/msmarco-MiniLM-L12-cos-v5', 'sentence-transformers/msmarco-MiniLM-L6-cos-v5', 'sentence-transformers/facebook-dpr-ctx_encoder-multiset-base', 'sentence-transformers/msmarco-bert-base-dot-v5', 'sentence-transformers/msmarco-distilbert-dot-v5', 'sentence-transformers/all-roberta-large-v1', 'sentence-transformers/paraphrase-distilroberta-base-v2', 'sentence-transformers/all-mpnet-base-v1', 'sentence-transformers/paraphrase-mpnet-base-v2', 'sentence-transformers/paraphrase-MiniLM-L12-v2', 'sentence-transformers/paraphrase-MiniLM-L6-v2', 'sentence-transformers/paraphrase-MiniLM-L3-v2', 'sentence-transformers/all-MiniLM-L12-v2', 'sentence-transformers/all-MiniLM-L12-v1', 'sentence-transformers/all-MiniLM-L6-v2', 'sentence-transformers/all-MiniLM-L6-v1', 'sentence-transformers/all-distilroberta-v1', 'sentence-transformers/multi-qa-mpnet-base-cos-v1', 'sentence-transformers/msmarco-distilbert-base-tas-b', 'sentence-transformers/multi-qa-mpnet-base-dot-v1', 'sentence-transformers/multi-qa-distilbert-cos-v1', 'sentence-transformers/multi-qa-MiniLM-L6-cos-v1', 'sentence-transformers/multi-qa-distilbert-dot-v1', 'sentence-transformers/multi-qa-MiniLM-L6-dot-v1', 'sentence-transformers/xlm-r-large-en-ko-nli-ststb', 'sentence-transformers/xlm-r-distilroberta-base-paraphrase-v1', 'sentence-transformers/xlm-r-bert-base-nli-stsb-mean-tokens', 'sentence-transformers/xlm-r-bert-base-nli-mean-tokens', 'sentence-transformers/xlm-r-base-en-ko-nli-ststb', 'sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens', 'sentence-transformers/xlm-r-100langs-bert-base-nli-mean-tokens', 'sentence-transformers/stsb-xlm-r-multilingual', 'sentence-transformers/stsb-roberta-large', 'sentence-transformers/stsb-roberta-base', 'sentence-transformers/stsb-roberta-base-v2', 'sentence-transformers/stsb-mpnet-base-v2', 'sentence-transformers/stsb-distilroberta-base-v2', 'sentence-transformers/stsb-distilbert-base', 'sentence-transformers/stsb-bert-large', 'sentence-transformers/stsb-bert-base', 'sentence-transformers/roberta-large-nli-stsb-mean-tokens', 'sentence-transformers/roberta-large-nli-mean-tokens', 'sentence-transformers/roberta-base-nli-stsb-mean-tokens', 'sentence-transformers/roberta-base-nli-mean-tokens', 'sentence-transformers/quora-distilbert-multilingual', 'sentence-transformers/quora-distilbert-base', 'sentence-transformers/paraphrase-xlm-r-multilingual-v1', 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2', 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2', 'sentence-transformers/paraphrase-distilroberta-base-v1', 'sentence-transformers/paraphrase-albert-small-v2', 'sentence-transformers/paraphrase-albert-base-v2', 'sentence-transformers/paraphrase-TinyBERT-L6-v2', 'sentence-transformers/nq-distilbert-base-v1', 'sentence-transformers/nli-roberta-large', 'sentence-transformers/nli-roberta-base', 'sentence-transformers/nli-roberta-base-v2', 'sentence-transformers/nli-mpnet-base-v2', 'sentence-transformers/nli-distilroberta-base-v2', 'sentence-transformers/nli-distilbert-base', 'sentence-transformers/nli-distilbert-base-max-pooling', 'sentence-transformers/nli-bert-large', 'sentence-transformers/nli-bert-large-max-pooling', 'sentence-transformers/nli-bert-large-cls-pooling', 'sentence-transformers/nli-bert-base', 'sentence-transformers/nli-bert-base-max-pooling', 'sentence-transformers/nli-bert-base-cls-pooling', 'sentence-transformers/msmarco-roberta-base-v3', 'sentence-transformers/msmarco-roberta-base-v2', 'sentence-transformers/msmarco-roberta-base-ance-firstp', 'sentence-transformers/msmarco-distilroberta-base-v2', 'sentence-transformers/msmarco-distilbert-multilingual-en-de-v2-tmp-trained-scratch', 'sentence-transformers/msmarco-distilbert-multilingual-en-de-v2-tmp-lng-aligned', 'sentence-transformers/msmarco-distilbert-base-v4', 'sentence-transformers/msmarco-distilbert-base-v3', 'sentence-transformers/msmarco-distilbert-base-v2', 'sentence-transformers/msmarco-distilbert-base-dot-prod-v3', 'sentence-transformers/msmarco-MiniLM-L-6-v3', 'sentence-transformers/msmarco-MiniLM-L-12-v3', 'sentence-transformers/facebook-dpr-question_encoder-single-nq-base', 'sentence-transformers/facebook-dpr-question_encoder-multiset-base', 'sentence-transformers/facebook-dpr-ctx_encoder-single-nq-base', 'sentence-transformers/distiluse-base-multilingual-cased', 'sentence-transformers/distiluse-base-multilingual-cased-v2', 'sentence-transformers/distiluse-base-multilingual-cased-v1', 'sentence-transformers/distilroberta-base-paraphrase-v1', 'sentence-transformers/distilroberta-base-msmarco-v2', 'sentence-transformers/distilroberta-base-msmarco-v1', 'sentence-transformers/distilbert-multilingual-nli-stsb-quora-ranking', 'sentence-transformers/distilbert-base-nli-stsb-quora-ranking', 'sentence-transformers/distilbert-base-nli-stsb-mean-tokens', 'sentence-transformers/distilbert-base-nli-mean-tokens', 'sentence-transformers/distilbert-base-nli-max-tokens', 'sentence-transformers/clip-ViT-B-32-multilingual-v1', 'sentence-transformers/bert-large-nli-stsb-mean-tokens', 'sentence-transformers/bert-large-nli-mean-tokens', 'sentence-transformers/bert-large-nli-max-tokens', 'sentence-transformers/bert-large-nli-cls-token', 'sentence-transformers/bert-base-wikipedia-sections-mean-tokens', 'sentence-transformers/bert-base-nli-stsb-mean-tokens', 'sentence-transformers/bert-base-nli-mean-tokens', 'sentence-transformers/bert-base-nli-max-tokens', 'sentence-transformers/bert-base-nli-cls-token', 'sentence-transformers/average_word_embeddings_levy_dependency', 'sentence-transformers/average_word_embeddings_komninos', 'sentence-transformers/average_word_embeddings_glove.840B.300d', 'sentence-transformers/average_word_embeddings_glove.6B.300d', 'sentence-transformers/allenai-specter', 'sentence-transformers/LaBSE']
 
 #model = SentenceTransformer('stsb-roberta-large')
-tool = language_tool_python.LanguageTool('en-US')
-
+#tool = language_tool_python.LanguageTool('en-US')
+'''
 def correct(original,sentenses):       
     frase = tool.correct(original)
 
@@ -24,7 +24,7 @@ def correct(original,sentenses):
         aux.append(tool.correct(i))
 
     return frase, aux
-
+'''
 def create_sentences(lista, biblioteca):
     lista_a = []
     for i in lista:
@@ -53,12 +53,13 @@ def home(request):
         lemmatization = [[token.lemma_ for token in conference_help_doc]]
         semantic = wordnet.busca_semantica(lemmatization)
         data['semantic'] = semantic
-        original, sentences = create_sentences(lemmatization[0], semantic[0])
-        data['original'], data['sentences'] = correct(original, sentences)
+        data['original'], data['sentences'] = create_sentences(lemmatization[0], semantic[0])
+        #data['original'], data['sentences'] = correct(original, sentences)
         output = {}
 
         output[''] = data['sentences']
-
+        return render(request,'results.html',data)
+    '''
         for sentence_transformer in sentence_transformers:
             output[sentence_transformer] = query({
                 "inputs": {
@@ -79,6 +80,6 @@ def home(request):
         #a_file.close()
         
         return response
-       
-        #return render(request,'results.html',data)
+       '''
+        
     return render(request,'index.html')
