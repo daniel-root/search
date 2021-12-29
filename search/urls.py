8053068d4351ebbfcdd41670fe6d81e302d001f7
daniel-albuquerque <albuquerquedaniel898@gmail.com>
2021-12-29 15:27:34 -0300

    delete

--- search/urls.py
+++ search/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from semantic.views import  home,search_api
+from semantic.views import  home
 
 urlpatterns = [
     path('', home,name="home"),
-    path('api', search_api,name="api"),
 ]
