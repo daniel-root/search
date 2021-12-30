5b049f15782786f078470769cb1401da458aca5d
daniel-albuquerque <albuquerquedaniel898@gmail.com>
2021-12-29 21:20:41 -0300

    fix

--- semantic/views.py
+++ semantic/views.py
@@ -11,77 +11,9 @@ suffix_regex = spacy.util.compile_suffix_regex(suffixes)
 nlp.tokenizer.suffix_search = suffix_regex.search
 #tool = language_tool_python.LanguageTool('en-US')
 
-sentence_transformers = ['albert-base-v1',
- 'albert-base-v2',
- 'albert-large-v1',
- 'albert-large-v2',
- 'albert-xlarge-v1',
- 'albert-xlarge-v2',
- 'albert-xxlarge-v1',
- 'albert-xxlarge-v2',
- 'bert-base-cased-finetuned-mrpc',
- 'bert-base-cased',
- 'bert-base-chinese',
- 'bert-base-german-cased',
- 'bert-base-german-dbmdz-cased',
- 'bert-base-german-dbmdz-uncased',
- 'bert-base-multilingual-cased',
- 'bert-base-multilingual-uncased',
- 'bert-base-uncased',
- 'bert-large-cased-whole-word-masking-finetuned-squad',
- 'bert-large-cased-whole-word-masking',
- 'bert-large-cased',
- 'bert-large-uncased-whole-word-masking-finetuned-squad',
- 'bert-large-uncased-whole-word-masking',
- 'bert-large-uncased',
- 'camembert-base',
- 'ctrl',
- 'distilbert-base-cased-distilled-squad',
- 'distilbert-base-cased',
- 'distilbert-base-german-cased',
- 'distilbert-base-multilingual-cased',
- 'distilbert-base-uncased-distilled-squad',
- 'distilbert-base-uncased-finetuned-sst-2-english',
- 'distilbert-base-uncased',
- 'distilgpt2',
- 'distilroberta-base',
- 'gpt2-large',
- 'gpt2-medium',
- 'gpt2-xl',
- 'gpt2',
- 'openai-gpt',
- 'roberta-base-openai-detector',
- 'roberta-base',
- 'roberta-large-mnli',
- 'roberta-large-openai-detector',
- 'roberta-large',
- 't5-11b',
- 't5-3b',
- 't5-base',
- 't5-large',
- 't5-small',
- 'transfo-xl-wt103',
- 'xlm-clm-ende-1024',
- 'xlm-clm-enfr-1024',
- 'xlm-mlm-100-1280',
- 'xlm-mlm-17-1280',
- 'xlm-mlm-en-2048',
- 'xlm-mlm-ende-1024',
- 'xlm-mlm-enfr-1024',
- 'xlm-mlm-enro-1024',
- 'xlm-mlm-tlm-xnli15-1024',
- 'xlm-mlm-xnli15-1024',
- 'xlm-roberta-base',
- 'xlm-roberta-large-finetuned-conll02-dutch',
- 'xlm-roberta-large-finetuned-conll02-spanish',
- 'xlm-roberta-large-finetuned-conll03-english',
- 'xlm-roberta-large-finetuned-conll03-german',
- 'xlm-roberta-large',
- 'xlnet-base-cased',
- 'xlnet-large-cased']
 
-def sentence_scores(original, sentences, model):    
-    model = SentenceTransformer(model)
+def sentence_scores(original, sentences):    
+    model = SentenceTransformer('all-MiniLM-L6-v2')
 
     embeddings1 = model.encode(original, convert_to_tensor=True)
     embeddings2 = model.encode(sentences, convert_to_tensor=True)
@@ -180,8 +112,8 @@ def home(request):
 
         output = {}
 
-        for sentence_transformer in sentence_transformers:
-            output[sentence_transformer] = sentence_scores(data['original'], data['sentences'], sentence_transformer)
+        #for sentence_transformer in sentence_transformers
+        output['all-MiniLM-L6-v2'] = sentence_scores(data['original'], data['sentences'])
 
         lista = list(output.values())[1:]
         total = len(output)
