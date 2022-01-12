3f57d2dc3d592f80999a4f19b2bbea05d69ab12f
daniel-albuquerque <albuquerquedaniel898@gmail.com>
2022-01-12 09:25:19 -0300

    api

--- semantic/views.py
+++ semantic/views.py
@@ -117,14 +117,14 @@ def home(request):
 
         #original, all_results = correct(original,all_results)
         
-        data['original'] = search
+        data['original'] = request.POST['search']
         data['sentences'] = {}
 
         #output = {}
 
         lista = query({
                 "inputs": {
-                    "source_sentence": search,
+                    "source_sentence": request.POST['search'],
                     "sentences": list(all_results)
                 },
             },'all-MiniLM-L6-v2')
@@ -155,6 +155,96 @@ def home(request):
     data['vote'] = round((vote.like*100) / vote.total, 2)    
 
     return render(request,'index.html',data)
+
+def api(request):
+    data = {}
+    if request.POST:
+        search = request.POST['search']
+        conference_help_doc = nlp(search)
+
+        #Tokenalização e Lematização
+        lemmatization, token = [token.lemma_ for token in conference_help_doc],[token.norm_ for token in conference_help_doc]
+
+        #N-grams
+        unigrams = [[i] for i in lemmatization]
+        bigrams = getNGrams(lemmatization,2)
+
+        #Token n-gramas
+        token_uni = [[i] for i in token]
+        token_bi = getNGrams(token,2)
+
+        #N-grams
+        grams = unigrams + bigrams
+        
+        search = ' '.join(lemmatization) + ' '
+
+        token_grams = []
+
+        for i in range(len(unigrams),1,-1):
+            for tokens in permutations(grams,i):
+                string = ''
+                for token in tokens:
+                    string += ' '.join(token) + ' '
+                if string == search:
+                    token_grams.append(list(tokens))
+
+        semantic = wordnet.busca_semantica([grams])
+
+        originals = []
+        all_results = []
+
+        a = token_uni + token_bi
+
+        teste = {}
+
+        for i in range(len(a)):
+            teste[' '.join(grams[i])] = ' '.join(a[i])
+        
+        for i in semantic[0].keys():
+            semantic[0][i].append(teste[i])
+
+        for gram in token_grams:
+            original, results = create_sentences([' '.join(i) for i in gram],semantic[0])
+            originals.append(gram)
+            all_results.extend(results)
+        
+        all_results = set(all_results)
+
+        #original, all_results = correct(original,all_results)
+        
+        data['original'] = request.POST['search']
+        data['sentences'] = {}
+
+        #output = {}
+
+        lista = query({
+                "inputs": {
+                    "source_sentence": request.POST['search'],
+                    "sentences": list(all_results)
+                },
+            },'all-MiniLM-L6-v2')
+
+        if not isinstance(lista,list):
+            vote = Vote.objects.get(id=1)
+            data['vote'] = round((vote.like*100) / vote.total, 2)
+            messages.info(request, 'There was an error performing your search. Please try again.')  
+            return render(request,'index.html',data)
+        #for sentence_transformer in sentence_transformers
+        #output['all-MiniLM-L6-v2'] = sentence_scores(data['original'], data['sentences'])
+
+        all_results = list(all_results)
+
+        #lista = list(output.values())
+        total = len(lista)
+        for i in range(total):          
+            data['sentences'][all_results[i]] = round(lista[i], 2)
+        
+        data['sentences'] = {k: v for k, v in sorted(data['sentences'].items(), reverse=True, key=lambda item: item[1])}
+
+        return JsonResponse(data)
+
+       
+
 
 def voting(request):
     if request.method == "POST":
